805504fa023e6c2c9aabea43914a100e4371f251
simona321 <simonafani3@gmail.com>
2020-05-31 17:41:08 +0200

    ndryshime

--- FqProfilit.php
+++ FqProfilit.php
@@ -24,7 +24,11 @@ require_once ("lidhjameDB.php");
 
 <?php
 //if(isset($_POST['first']) && isset($_POST['last'])) lidhja me afishi
-$ID=$_GET['GetID'];
+// $ID=$_GET['GetID'];
+$ID = ''; 
+if( isset( $_GET['GetID'])) {
+    $ID = $_GET['GetID']; 
+} 
 $query = "SELECT * FROM tbluser WHERE ID = '".$ID."'";
 $result=mysqli_query($conn,$query);
 
